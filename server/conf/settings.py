e6ed4f9e2a6e4b16d6ec62619ec9b466655fd545
DanielBash <ddbash@icloud.com>
2025-12-11 22:22:16 +0300

    - Minor fix

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -37,7 +37,6 @@ SERVERNAME = "worm-rpg"
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
 
-
 ALLOWED_HOSTS = ['worm.ibashlhr.beget.tech', 'localhost', '127.0.0.1']
 
 WEBSOCKET_CLIENT_URL = "wss://worm.ibashlhr.beget.tech/ws"
