7f428dbdcbb23d11ffa7a75e81aa5d0adbb9771b
DanielBash <ddbash@icloud.com>
2025-12-12 07:12:22 +0300

    - Added AMP port

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -7,6 +7,8 @@ ALLOWED_HOSTS = ['worm.ibashlhr.beget.tech', 'localhost', '127.0.0.1']
 
 WEBSOCKET_CLIENT_URL = "wss://worm.ibashlhr.beget.tech/ws"
 
+AMP_PORT = 4006
+
 try:
     from server.conf.secret_settings import *
 except ImportError:
