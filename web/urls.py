7670a434724901cf121c72fdf54bb040ed2e0c64
DanielBash <ddbash@icloud.com>
2025-12-20 10:17:21 +0300

    - Improved website a little

--- web/urls.py
+++ web/urls.py
@@ -21,13 +21,14 @@ from evennia.web.urls import urlpatterns as evennia_default_urlpatterns
 # add patterns
 urlpatterns = [
     # website
-    path("", include("web.website.urls")),
+    # path("", include("web.website.urls")),
     # webclient
-    path("webclient/", include("web.webclient.urls")),
+    #path("webclient/", include("web.webclient.urls")),
     # web admin
-    path("admin/", include("web.admin.urls")),
+    #path("admin/", include("web.admin.urls")),
     # add any extra urls here:
     # path("mypath/", include("path.to.my.urls.file")),
+path("", include("web.webclient.urls")),
 ]
 
 # 'urlpatterns' must be named such for Django to find it.
